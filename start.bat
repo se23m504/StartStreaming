242d8055d8090e163cf5d75bee1074d35cdb0e81
Santiago Lo Coco <se23m504@technikum-wien.at>
2024-10-19 09:31:44 +0200

    Start programs minimized

--- start.bat
+++ start.bat
@@ -4,8 +4,7 @@ echo Enabling mobile hotspot
 powershell -NoProfile -ExecutionPolicy Bypass -File ".\mhotspot.ps1"
 
 echo Starting docker desktop
-start "" "C:\Program Files\Docker\Docker\Docker Desktop.exe"
-@REM timeout /t 5 >nul
+start /MIN "DockerDesktop" "C:\Program Files\Docker\Docker\Docker Desktop.exe"
 
 :waitForDocker
 tasklist | find /i "Docker Desktop.exe" >nul
@@ -26,8 +25,8 @@ cd ".\mediamtx"
 docker compose up -d
 
 echo Launching OBS instances
-start "OBS1" /D "C:\Program Files\obs-studio\bin\64bit" "obs64.exe" --profile "Profile1" --collection "Profile1" --multi --startstreaming
+start /MIN "OBS1" /D "C:\Program Files\obs-studio\bin\64bit" "obs64.exe" --profile "Profile1" --collection "Profile1" --multi --startstreaming
 timeout /t 1 >nul
-start "OBS2" /D "C:\Program Files\obs-studio\bin\64bit" "obs64.exe" --profile "Profile2" --collection "Profile2" --multi --startstreaming
+start /MIN "OBS2" /D "C:\Program Files\obs-studio\bin\64bit" "obs64.exe" --profile "Profile2" --collection "Profile2" --multi --startstreaming
 
 @REM pause
